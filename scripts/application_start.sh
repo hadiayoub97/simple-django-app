9179c95b2f416fe6a11f7992ad7251c2524a5d7d
hadiayoub <ayoub-hadi-1997@hotmail.com>
2024-08-31 17:39:47 +0300

    fix code deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Starting the application"
-cd /home/ec2-user/app
+cd /home/ec2-user/simple-django-app/cool_counters
+python3 manage.py migrate
 nohup python3 manage.py runserver 0.0.0.0:8000 &
