14e4de8151ce45a3198033d3ced60ced2721a335
hadiayoub <ayoub-hadi-1997@hotmail.com>
2024-08-31 19:26:16 +0300

    fix nohup output logs

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,4 @@
 echo "Starting the application"
 cd /home/ec2-user/simple-django-app/cool_counters
 python3 manage.py migrate
-nohup python3 manage.py runserver 0.0.0.0:8000 &
+nohup python3 manage.py runserver 0.0.0.0:8000 &> /dev/null &
